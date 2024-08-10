6af927f2e1a347c6d25a550dbaa798192e11529a
MRamya-sri <rsri40170@gmail.com>
2024-08-10 17:12:31 +0530

    first2

---
+++ Basic-Programs/Armstrong.py
@@ -0,0 +1,20 @@
+num = int(input("Enter the number:"))
+
+#declaring
+sum = 0
+count = len(str(num))
+
+#assigning temp to store num
+temp = num
+
+#loop and logic
+while temp > 0:
+    digit = temp % 10
+    sum += digit** count
+    temp //= 10
+
+# condition
+if num == sum:
+    print("its Armstrong!")
+else:
+    print("Its not Armstrong.")
