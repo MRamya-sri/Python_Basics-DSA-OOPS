8e2ad6233b116b55213701178564d00db9d003cd
MRamya-sri <rsri40170@gmail.com>
2024-09-14 19:03:16 +0530

    DSA

--- Python_Operations_&_DataTypes/Merging_Dictionaries.py
+++ Python_Operations_&_DataTypes/Merging_Dictionaries.py
@@ -8,8 +8,3 @@ print(dict1)
 merged_dict = dict1 | dict2
 print(merged_dict)
 
-pop = dict2.pop()
-print(pop)
-
-clear = dict1.clear()
-print(clear)
---
+++ Python_Operations_&_DataTypes/Pop_Dictionary.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+popped_value = my_dict.pop("age")
+print(popped_value)  # Output: 30
+print(my_dict)  
